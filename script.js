e3106ed189c7b513fc5ce48fee8b031838b7463b
TheRedQueen <pand3m1x@gmail.com>
2026-01-26 20:18:17 -0500

    ♫⋆｡♪ ₊˚♬ ﾟ Well, this is embarrasing, I couldn't figure out why my console wouldn't log anything, it's because I never saved and refresh ... ♫⋆｡♪ ₊˚♬ ﾟ

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
-let title = document.getElementById("title").value;
-let content = document.getElementById("content").value;
+// Create New Posts, with both fields filled requirement, posts displayed, error message []
+// Dynamically added posted posts that can be edited
+// Edit Posts with clear edit button, form validation
+// Delete Button that also works to remove from local storage
+// Data Persistance with Local Storage
 
+// let title = document.getElementById("title").value;
+// let content = document.getElementById("content").value;
+// document.addEventListener("DOMContentLoaded", loadPosts); this is for HTML code that don't have script at bottom
+
+//♫⋆｡♪ ₊˚♬ ﾟ.  Variables I want access to outside of greedy functions =-= ♫⋆｡♪ ₊˚♬ ﾟ.
+
+
+//♫⋆｡♪ ₊˚♬ ﾟ. Adding the Blog Post  ♫⋆｡♪ ₊˚♬ ﾟ.
+const postButton = document.getElementById("postButton");
+const postsContainer = document.getElementById("posts")
+
+postButton.addEventListener("click", addPost);
+
+function addPost() {
+  let title = document.getElementById("title").value;
+  let content = document.getElementById("content").value;
+
+    console.log("Trying to post:", title, content);
+
+  if (title === ""){
+    alert ("Please enter a title for your blog post 😀");
+    return;
+  }
+
+  if (content === ""){
+    alert ("Tell the world your thoughts by filling out the content box 😀");
+    return;
+  }
+//If both conditions are met the post should be made with below code 
+  if (title && content) {
+    const posts = JSON.parse(localStorage.getItem("posts")) || [];
+    posts.push({ title, content });
+    localStorage.setItem("posts", JSON.stringify(posts));
+
+  loadPosts();
+  console.log("posted!")
+  };
+};
+  
+  function loadPosts() {
+    const posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+    postsContainer.innerHTML = "";
+
+    posts.forEach(post => {
+      const div = document.createElement("div");
+      div.className = "post";
+      const h3 = document.createElement("h3");
+      h3.className = "post-title";
+      h3.textContent = post.title;
+      const p = document.createElement("p");
+      p.className = "post-content";
+      p.textContent = post.content;
+
+      div.append(h3, p);
+      postsContainer.appendChild(div);
+    });
+  };
+loadPosts();
+console.log("added post")
+
+//♫⋆｡♪ ₊˚♬ ﾟ. Editing the Blog Post ♫⋆｡♪ ₊˚♬ ﾟ.
+
+//♫⋆｡♪ ₊˚♬ ﾟ. Deleting the Blog Post  ♫⋆｡♪ ₊˚♬ ﾟ.
+
+//♫⋆｡♪ ₊˚♬ ﾟ. Ideas for Later! ♫⋆｡♪ ₊˚♬ ﾟ.
+//  Music player youtube.com/watch?v=qRkv3gPPRME - or - this one is probably better and less distracting https://www.youtube.com/watch?v=GGU7yCmhjRY
+//  Button that shoots out music notes when submit is hit
+//  Is it possible to make a background with emojis?
+//  adding a date stamp?
+//  adding -- Hachi to the end of every entry? Like as a sig
+
+//                            ෴⚘⎧ᴿᴵᴾ⎫⚘෴CODE GRAVEYARD ִֶָ෴⚘⎧ᴿᴵᴾ⎫⚘෴
